f34fd0f1e072d6e7b1d233754be3a1fe1e59ba1f
Yaroslav Gribov <yaroslavgribov@yandex.ru>
2017-07-18 02:13:37 +0300

    make posts take full page on scroll

--- content/themes/cinemaholics/assets/js/modules/scroll.js
+++ content/themes/cinemaholics/assets/js/modules/scroll.js
@@ -1,15 +1,35 @@
 let newsContainer = document.querySelector('#news'),
-    postsContainer = document.querySelector('#content');
+    postsContainer = document.querySelector('#content'),
+    postsInnerContainer = document.querySelector('.mateblock'),
+    allPosts = postsInnerContainer.querySelectorAll('.block'),
+    latestPost = allPosts[allPosts.length - 3],
+    latestPostBottom = latestPost.offsetTop,
+    bottomCache = 0;
 
 
 window.addEventListener('scroll', () => {
     if (newsContainer) {
         let newsBottom = newsContainer.getBoundingClientRect().bottom;
+        
+        let windowScrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-        if (newsBottom < 0) {
+        console.log(bottomCache, newsBottom, windowScrollTop, latestPostBottom);
+        
+        if (!bottomCache && newsBottom < 0) {
+            bottomCache = newsBottom + windowScrollTop;
+        }
+
+        if (bottomCache) {
+            postsContainer.style.paddingTop = 800 +'px';
             postsContainer.classList.add('full');
-        } else {
+            newsContainer.classList.add('hidden');
+        } 
+        
+        if (windowScrollTop < bottomCache) {
+            bottomCache = 0;
+            postsContainer.style.paddingTop = 0;
             postsContainer.classList.remove('full');
+            newsContainer.classList.remove('hidden');
         }
     }
 })
