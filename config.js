43928285c401a9690604a82b49410274ebca9a6b
Yaroslav Gribov <yaroslavgribov@yandex.ru>
2017-07-24 23:52:05 +0300

    final config

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://beta.cinemaholics.ru',
+        url: 'http://cinemaholics.ru',
         mail: {},
         database: {
             client: 'sqlite3',
